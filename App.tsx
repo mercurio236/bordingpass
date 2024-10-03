bc4d47b6e46edccb47cc08bdaa6aa417c49f3fe1
Arley Souto <macx15@hotmail.com>
2024-10-02 22:37:06 -0300

    feat: definido fonts e colors globais da aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,23 @@
+import {
+  Poppins_700Bold,
+  Poppins_500Medium,
+  Poppins_400Regular,
+  Poppins_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/poppins'
+
 import { Home } from '@/app/home'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+    Poppins_500Medium,
+    Poppins_400Regular,
+    Poppins_800ExtraBold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
   return <Home />
 }
